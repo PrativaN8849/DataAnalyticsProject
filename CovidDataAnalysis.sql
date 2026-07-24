a6c1cb8487b4e079c0ecd6db62116b8eacd94711
Prativa Nepal <nepalp260@gmail.com>
2026-07-24 21:47:14 +0200

    updated

--- CovidDataAnalysis.sql
+++ CovidDataAnalysis.sql
@@ -9,7 +9,7 @@ Order by location;
 
 Select COUNT( DISTINCT location) AS Total_Countries
 From [Portfolio Project].dbo.CovidDeaths
-WHERE location IS NOT NULL;
+WHERE location IS NOT NULL AND continent IS not NULL; --location also has continent in the data where corresponding continent value is NULL
 --
 
 --Important features
@@ -20,7 +20,7 @@ Order by 1,2;
 
 
 --Total Cases vs. Total Deaths
---How many cases and deaths(for entrire case) per country?
+--Out of all confirmed Covid cases, what percentage of infected people died?
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
 From [Portfolio Project].dbo.CovidDeaths
 Order by 1,2;
@@ -43,40 +43,40 @@ Order by 1,2;
 --
 
 --Total cases vs. Population
---What % of population got covid?
+--What percentage of population got covid?
 Select Location, date,population, total_cases, (total_cases/population)*100 AS 'infected_population%'
 From [Portfolio Project].dbo.CovidDeaths
-where location LIKE '%Nepal%'
+where location LIKE '%Germany%'
 Order by 1,2;
-
---What country has the highest infection count per location? --
-Select Location,population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS 'infected_population%'
-From [Portfolio Project].dbo.CovidDeaths
-Group by location, population
-Order by 'infected_population%';
-
--- Year and the Number of record --
-Select YEAR(date) AS Year, COUNT(*) AS Num_of_records
-From [Portfolio Project].dbo.CovidDeaths
-GROUP BY YEAR(date)
-ORDER BY Year;
 --
 
--- Why was the number of record huge in 2020 and not 2021? -- 
-Select YEAR(date) AS Year, MONTH(date) AS Month
+--What country has the largest proportion of its population infected? --
+Select Location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS 'infected_population%'
 From [Portfolio Project].dbo.CovidDeaths
-GROUP BY YEAR(date), Month(date)
-Order by Year, Month;
--- Comparing cumulative yearly statistics would be biased as 2021 has only 4 months record and 2020 with 12 months
--
+Group by location, population
+Order by 'infected_population%' desc;
+-- Germany : reached 3.4 million confirmed cases, i.e. at it's peak, 4.06% of it's population had been infected.
+-- A comparison is fair when it's compared to its own population rather than another contry's population
 
--- If so, how did the infection percentage change between "January–April 2020" and "January–April 2021"?
-Select location, YEAR(date) AS year, MAX(total_cases) AS max_total_case, MAX(population) as max_population,
-ROUND(MAX(total_cases) * 100.0/MAX(population),2) AS infection_percentage
+-- What couttry has the highest death count per cases?
+Select location, population, MAX (total_cases) AS HighestInfectionCount, MAX(cast(total_deaths as int)) AS HighestDeathCount, MAX(total_deaths)*100/ NULLIF(MAX(total_cases),0) AS 'DeathPercentage'
 From [Portfolio Project].dbo.CovidDeaths
-WHERE MONTH(date) BETWEEN 1 AND 4 
-AND YEAR(date) IN (2020,2021)
-AND continent IS NOT NULL
-GROUP BY location, YEAR(date)
-ORDER BY location, year;
---Each country is compared on 2020 and 2021 and results drawn--
+Where continent is not null  --the location consist of continent and null on corresponding continent
+Group by location, population
+order by Deathpercentage desc;
+
+--Not the largest percentage yet the largest population
+Select Location, MAX(cast(total_deaths as int)) AS HighestDeathCount
+From [Portfolio Project].dbo.CovidDeaths
+Where continent is not null 
+Group by location
+order by HighestDeathCount desc;
+--Germany comes on number 9 with 83097 deaths and Nepal on 67 with 3279 deaths, US has the highest i.e. 576232
+
+--Highest death count By Continent
+Select location, MAX(cast(total_deaths as int)) AS HighestDeathCount
+From [Portfolio Project].dbo.CovidDeaths
+Where continent is null  -- checking only the continent,[the data consist location as continent and its corresponding continent NULL]
+Group by location
+order by HighestDeathCount desc;
+
