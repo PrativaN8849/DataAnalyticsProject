cbffd855d7f6d6dd3ee79f550f4f47e4c94af459
Prativa Nepal <nepalp260@gmail.com>
2026-08-07 23:52:17 +0200

    Add files via upload

--- CovidDataAnalysis.sql
+++ CovidDataAnalysis.sql
@@ -1,64 +1,102 @@
 -- Extract and Explore the Data ---
-Select * From [Portfolio Project].dbo.CovidDeaths;
+SELECT * FROM [Portfolio Project].dbo.CovidDeaths
+ORDER BY 3,4;
+
+SELECT * FROM [Portfolio Project].dbo.CovidVaccination
+ORDER BY 3,4
 --
 
 -- List out the total country
-Select DISTINCT(location)
-From [Portfolio Project].dbo.CovidDeaths 
-Order by location;
+SELECT DISTINCT(location)
+FROM [Portfolio Project].dbo.CovidDeaths 
+ORDER BY location;
 
-Select COUNT( DISTINCT location) AS Total_Countries
-From [Portfolio Project].dbo.CovidDeaths
+SELECT COUNT( DISTINCT location) AS Total_Countries
+FROM [Portfolio Project].dbo.CovidDeaths
 WHERE location IS NOT NULL AND continent IS not NULL; --location also has continent in the data where corresponding continent value is NULL
 --
 
 --Important features
-Select Location, date, population, total_cases, new_cases, total_deaths
-From [Portfolio Project].dbo.CovidDeaths
-Order by 1,2;
+SELECT Location, date, population, total_cases, new_cases, total_deaths
+FROM [Portfolio Project].dbo.CovidDeaths
+ORDER BY 1,2;
 --
 
 
 --Total Cases vs. Total Deaths
 --Out of all confirmed Covid cases, what percentage of infected people died?
-Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
-From [Portfolio Project].dbo.CovidDeaths
-Order by 1,2;
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
+FROM [Portfolio Project].dbo.CovidDeaths
+ORDER BY 1,2;
 -- In Afghanistan when 34 cases detected , total death was 1 and 2.94%
---the last record from 2021_04_30 shows 59745 total cases from which 2625 were dead which has 4.39% i.e. 4 % chance of death
+-- the last record from 2021_04_30 shows 59745 total covid cases from which 2625 were dead which has 4.39% i.e. 4 % chance of death
 
 
---checking the same for Germany
-Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
-From [Portfolio Project].dbo.CovidDeaths
-where location LIKE '%Germany%'
-Order by 1,2;
--- In 2021 Apr, Germany has 2% chance of death i.e. out of 3405365 , 83097 were dead nevertheless high number of people died comapred to Afghanistan
+--checking the death percentage for Germany
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
+FROM [Portfolio Project].dbo.CovidDeaths
+WHERE location LIKE '%Germany%'
+ORDER BY 1,2;
+-- In 2021_4_30 Apr, Germany has 2.4% chance of death i.e. out of 3405365 covid cases, 83097 were dead nevertheless high number of people died comapred to Afghanistan
 
---Nepal
-Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
-From [Portfolio Project].dbo.CovidDeaths
-where location LIKE '%Nepal%'
-Order by 1,2;
+--checking the death percentage for Nepal
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS 'death_percentage'
+FROM [Portfolio Project].dbo.CovidDeaths
+WHERE location LIKE '%Nepal%'
+ORDER BY 1,2;
+-- In 2021 Apr, Nepal has 1.01% chance of death i.e. 3279 deaths out of 323187 --
+
+-- Since the covid case of Germany is already higher and so the death number the death percentage will be high if compared to Nepal 
+
+
+--Check dtype of each columns--
+EXEC sp_help '[Portfolio Project].dbo.CovidDeaths';
 --
 
---Total cases vs. Population
---What percentage of population got covid?
-Select Location, date,population, total_cases, (total_cases/population)*100 AS 'infected_population%'
-From [Portfolio Project].dbo.CovidDeaths
-where location LIKE '%Germany%'
-Order by 1,2;
+-- Out of total case, what percent of people died in Germany and Nepal? --
+Select location, date, total_cases, total_deaths,
+    TRY_CAST(total_deaths AS FLOAT) * 100.0 / NULLIF(TRY_CAST(total_cases AS FLOAT), 0) --total deaths and total_cases are numeric
+    AS case_fatality_percentage
+FROM [Portfolio Project].dbo.CovidDeaths
+WHERE location IN ('Germany', 'Nepal')
+  AND date = '2021-04-30'
+ORDER BY case_fatality_percentage DESC;
 --
 
---What country has the largest proportion of its population infected? --
-Select Location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS 'infected_population%'
+-- Total Population in Germany and Nepal
+SELECT location, MAX(Population)AS Total_Population
+FROM [Portfolio Project].dbo.CovidDeaths 
+WHERE location IN ('Germany', 'Nepal')
+GROUP BY location;
+--
+
+-- Which country had the higher number of deaths per 100,000 population — Germany or Nepal?
+SELECT location, date, population,
+    TRY_CAST(total_deaths AS FLOAT) AS total_deaths,
+    
+    (TRY_CAST(total_deaths AS FLOAT) / NULLIF(population, 0)) * 100
+        AS population_death_percentage,
+
+    (TRY_CAST(total_deaths AS FLOAT) / NULLIF(population, 0)) * 100000
+        AS deaths_per_100000_population
+
+FROM [Portfolio Project].dbo.CovidDeaths
+WHERE location IN ('Germany', 'Nepal')
+AND date = '2021-04-30'
+ORDER BY deaths_per_100000_population DESC;
+--
+
+
+--What country has the highest infection rate compared to population? --
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS 'infected_population%'
 From [Portfolio Project].dbo.CovidDeaths
 Group by location, population
 Order by 'infected_population%' desc;
--- Germany : reached 3.4 million confirmed cases, i.e. at it's peak, 4.06% of it's population had been infected.
--- A comparison is fair when it's compared to its own population rather than another contry's population
+-- Andorra has the highest infected population with 17.13%
+-- Germany : reached 3.4 million confirmed cases, i.e. at it's peak, 4.06% of it's population had been infected | while Nepal recorded 323,187 cases (1.11% of its population)
 
--- What couttry has the highest death count per cases?
+
+-- What country has the highest death count per cases?
 Select location, population, MAX (total_cases) AS HighestInfectionCount, MAX(cast(total_deaths as int)) AS HighestDeathCount, MAX(total_deaths)*100/ NULLIF(MAX(total_cases),0) AS 'DeathPercentage'
 From [Portfolio Project].dbo.CovidDeaths
 Where continent is not null  --the location consist of continent and null on corresponding continent
